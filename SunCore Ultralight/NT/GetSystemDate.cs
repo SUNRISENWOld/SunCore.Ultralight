9390c15c4cd8d5a230d20352e42bb60a2ba410c1
EgeLosing <77616044+EgeLosing@users.noreply.github.com>
2022-08-16 13:59:11 +0300

    Add files via upload

---
+++ SunCore Ultralight/NT/GetSystemDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SunCore.Ultralight.NT
+{
+    public static class GetSystemDate
+    {
+        public static string GetDateAsString(string Type)
+        {
+            return DateTime.Now.ToString(Type);
+        }
+
+        public static DateTime GetDateAsDateTime(string Type)
+        {
+            return Convert.ToDateTime(GetDateAsString(Type));
+        }
+    }
+}
---
+++ SunCore Ultralight/NT/Media/MP3Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SunCore.Ultralight.NT.Media
+{
+    public class MP3Player
+    {
+        [DllImport("Winmm.dll", SetLastError = true)]
+        static extern int mciSendString(string lpszCommand, [MarshalAs(UnmanagedType.LPStr)] StringBuilder lpszReturnString, int cchReturn, IntPtr hwndCallback);
+
+        public void Play(string FileLocation)
+        {
+            StringBuilder sb = new StringBuilder();
+            string sFileName = FileLocation;
+            string sAliasName = "MP3";
+            int nRet = mciSendString("open \"" + sFileName + "\" alias " + sAliasName, sb, 0, IntPtr.Zero);
+            nRet = mciSendString("play " + sAliasName, sb, 0, IntPtr.Zero);
+        }
+    }
+}
